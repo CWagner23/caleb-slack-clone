a351db7e256af0988e845ecb094d5b71650553ca
CWagner23 <calebwagner23@gmail.com>
2019-05-15 14:50:45 -0400

    Added functionality to login page

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,9 +1,79 @@
 import React from 'react'
 
+import {Grid, Form, Segment, Button, Header, Message, Icon} from 'semantic-ui-react';
+import {Link} from 'react-router-dom';
+
+import firebase from '../../firebase';
 class Login extends React.Component {
+    state = {
+        email: '',
+        password: '',
+        errors: [], 
+        loading: false, 
+    };
+
+    displayErrors = errors => {
+        return errors.map((error, i) => <p key={i}>{error.message}</p>);
+    }
+
+    handleInputErrors = (errors, inputName) => {
+        return errors.some(error => error.message.toLowerCase().includes(inputName)) ? "error" : "";
+    }
+    handleChange = event => {
+        this.setState({ [event.target.name]: event.target.value})
+    };
+
+    handleSubmit = event => {
+        event.preventDefault();
+        if(this.isFormValid(this.state)){    
+            this.setState({errors: [], loading: true});
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(this.state.email, this.state.password)
+                .then(signedInUser => {
+                    console.log(signedInUser);
+                    this.setState({loading: false});
+                })
+                .catch(err => {
+                    console.error(err);
+                    this.setState({loading: false, 
+                        errors: this.state.errors.concat(err)
+                    });
+                });
+        }
+        
+    }
+
+    isFormValid = ({email, password}) => email && password;
+
     render () {
+        const {email, password, errors, loading} = this.state;
         return (
-            <div>Login</div>
+            <Grid textAlign="center" verticalAlign="middle" className="app">
+                <Grid.Column style={{ maxWidth: 450}}>
+                    <Header as="h1" icon color="violet" textAlign="center">
+                        <Icon name="code branch" color="violet"/>
+                        Login to Caleb Slack Clone
+                    </Header>
+                    <Form size="large" onSubmit={this.handleSubmit}>
+                        <Segment stacked>
+                            <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="Email Address" onChange={this.handleChange} type="email" value={email} className={this.handleInputErrors(errors, 'email')}/>
+                            <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="Password" onChange={this.handleChange} type="password" value={password} className={this.handleInputErrors(errors, 'password')}/>
+                            <Button disabled={loading} className={loading ? 'loading': ''} color="violet" fluid size="large">Submit</Button>
+
+                        </Segment>
+                    </Form>
+                    {errors.length > 0 &&
+                        <Message error>
+                            <h3>Error</h3>
+                            {this.displayErrors(errors)}
+                        </Message>
+                    }
+                    <Message>Don't have an account? {" "} 
+                        <Link to="/register">Register</Link>
+                    </Message>
+                </Grid.Column>
+            </Grid>
         )
     }
 }
